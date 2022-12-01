57700ea84e25de9dd9c4cf743ca3168e6f54ad70
edmundykw <edmundykw@gmail.com>
2022-12-01 21:25:09 +0800

    practice

--- compressed_mean.sql
+++ compressed_mean.sql
@@ -12,6 +12,6 @@ Credits: https://www.linkedin.com/in/nick-singh-tech
 
 /*My solution:*/
 SELECT 
-    ROUND(SUM(item_count * order_occurrences) * 100.0 / 
-    SUM(order_occurrences) / 100.0, 1) AS avg_item
+    ROUND(SUM(CAST(item_count AS decimal) * order_occurrences) / 
+    SUM(order_occurrences), 1) AS avg_item
 FROM items_per_order;
