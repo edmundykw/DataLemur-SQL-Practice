8229b1e7ca6b1cd7ceb5c62e6987e4ad4a9b2446
edmundykw <edmundykw@gmail.com>
2022-12-01 19:22:09 +0800

    practice

---
+++ compressed_mean.sql
@@ -0,0 +1,17 @@
+/*
+Question: 
+
+You are trying to find the mean number of items bought per order on Alibaba, rounded to 1 decimal place.
+However, instead of doing analytics on all Alibaba orders, you have access to a summary table,
+which describes how many items were in an order (item_count),
+and the number of orders that had that many items (order_occurrences).
+
+Source: https://datalemur.com/questions/alibaba-compressed-mean
+Credits: https://www.linkedin.com/in/nick-singh-tech
+*/
+
+/*My solution:*/
+SELECT 
+    ROUND(SUM(item_count * order_occurrences) * 100.0 / 
+    SUM(order_occurrences) / 100.0, 1) AS avg_item
+FROM items_per_order;
