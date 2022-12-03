b0df8a9d423e4b57f8c3b68d8401ba5c7c0c7217
edmundykw <edmundykw@gmail.com>
2022-12-03 10:38:14 +0800

    practice

--- top-profitable-drugs_p4.sql
+++ top-profitable-drugs_p4.sql
@@ -9,7 +9,7 @@ Source: https://datalemur.com/questions/top-drugs-sold
 Credits: https://www.linkedin.com/in/nick-singh-tech
 */
 
-/*My solution:*/
+/*My solution using subquery:*/
 SELECT ranks.manufacturer, drug
 FROM
   (SELECT *,
@@ -18,3 +18,15 @@ FROM
     ORDER BY units_sold DESC) 
   FROM pharmacy_sales) AS ranks
 WHERE ranks.rank < 3
+
+/*My solution using CTE*/
+WITH ranks AS (
+  SELECT *,
+  rank() OVER (
+    PARTITION BY manufacturer 
+    ORDER BY units_sold DESC) 
+  FROM pharmacy_sales)
+  
+SELECT manufacturer, drug
+FROM ranks
+WHERE rank < 3;
